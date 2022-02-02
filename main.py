2a5e057713d46156d47a09c4b50216137d3c485a
leftthomas <leftthomas@qq.com>
2022-02-02 18:11:01 +0800

    finish train loop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-import time
 
 import pandas as pd
 import torch
+import torch.nn.functional as F
 from PIL import Image
 from torch.optim import Adam
 from torch.optim.lr_scheduler import MultiStepLR
@@ -14,56 +14,29 @@ from rcdnet import RCDNet
 from utils import parse_args, RainDataset
 
 
-def train_loop(net, data_loader):
+def train_loop(net, data_loader, n_iter):
     net.train()
-    step = 0
-    for epoch in range(1, args.num_iter):
-        mse_per_epoch = 0
-        tic = time.time()
-        # train stage
-        lr = optimizer.param_groups[0]['lr']
-        phase = 'train'
-        for ii, data in enumerate(data_loader):
-            im_rain, im_gt = [x.cuda() for x in data]
-            optimizer.zero_grad()
-            B0, ListB, ListR = net(im_rain)
-            loss_Bs = 0
-            loss_Rs = 0
-            for j in range(opt.num_stage):
-                loss_Bs = float(loss_Bs) + 0.1 * F.mse_loss(ListB[j], im_gt)
-                loss_Rs = float(loss_Rs) + 0.1 * F.mse_loss(ListR[j], im_rain - im_gt)
-            lossB = F.mse_loss(ListB[-1], im_gt)
-            lossR = 0.9 * F.mse_loss(ListR[-1], im_rain - im_gt)
-            lossB0 = 0.1 * F.mse_loss(B0, im_gt)
-            loss = lossB0 + loss_Bs + lossB + loss_Rs + lossR
-            # back propagation
-            loss.backward()
-            optimizer.step()
-            mse_iter = loss.item()
-            mse_per_epoch += mse_iter
-            if ii % 300 == 0:
-                template = '[Epoch:{:>2d}/{:<2d}] {:0>5d}/{:0>5d}, Loss={:5.2e}, lr={:.2e}'
-                print(template.format(epoch + 1, opt.niter, ii, num_iter_epoch, mse_iter, lr))
-            step += 1
-        mse_per_epoch /= (ii + 1)
-        print('Epoch:{:>2d}, Derain_Loss={:+.2e}'.format(epoch + 1, mse_per_epoch))
-        # adjust the learning rate
-        lr_scheduler.step()
-        # save model
-        model_prefix = 'model_'
-        save_path_model = os.path.join(opt.model_dir, model_prefix + str(epoch + 1))
-        torch.save({
-            'epoch': epoch + 1,
-            'step': step + 1,
-        }, save_path_model)
-        model_prefix = 'DerainNet_state_'
-        save_path_model = os.path.join(opt.model_dir, model_prefix + str(epoch + 1) + '.pt')
-        torch.save(net.state_dict(), save_path_model)
-        toc = time.time()
-        print('This epoch take time {:.2f}'.format(toc - tic))
-        print('-' * 100)
-    print('Reach the maximal epochs! Finish training')
-    return loss
+    total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader, initial=1, dynamic_ncols=True)
+    for rain, norain, name in train_bar:
+        rain, norain = rain.cuda(), norain.cuda()
+        b_0, list_b, list_r = net(rain)
+        loss_bs, loss_rs = 0.0, 0.0
+        for j in range(args.num_stage):
+            loss_bs = loss_bs + 0.1 * F.mse_loss(list_b[j], norain)
+            loss_rs = loss_rs + 0.1 * F.mse_loss(list_r[j], rain - norain)
+        loss_b = F.mse_loss(list_b[-1], norain)
+        loss_r = F.mse_loss(list_r[-1], rain - norain)
+        loss_b0 = F.mse_loss(b_0, norain)
+        loss = 0.1 * loss_b0 + loss_bs + loss_b + loss_rs + 0.9 * loss_r
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_num += rain.size(0)
+        total_loss += loss.item() * rain.size(0)
+        train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'
+                                  .format(n_iter, args.num_iter, total_loss / total_num))
+    return total_loss / total_num
 
 
 def test_loop(net, data_loader, n_iter):
@@ -89,7 +62,7 @@ def test_loop(net, data_loader, n_iter):
             if not os.path.exists(os.path.dirname(save_path)):
                 os.makedirs(os.path.dirname(save_path))
             Image.fromarray(out.squeeze(dim=0).permute(1, 2, 0).cpu().numpy()).save(save_path)
-            test_bar.set_description('Test [{}/{}] PSNR: {:.2f} SSIM: {:.4f}'
+            test_bar.set_description('Test Epoch: [{}/{}] PSNR: {:.2f} SSIM: {:.4f}'
                                      .format(n_iter, 1 if args.model_file else args.num_iter,
                                              total_psnr / count, total_ssim / count))
     return total_psnr / count, total_ssim / count
@@ -113,9 +86,7 @@ if __name__ == '__main__':
     test_dataset = RainDataset(args.data_path, args.data_name, 'test')
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=args.workers)
 
-    results = {'PSNR': [], 'SSIM': []}
-    best_psnr, best_ssim = 0.0, 0.0
-
+    results, best_psnr, best_ssim = {'PSNR': [], 'SSIM': []}, 0.0, 0.0
     model = RCDNet(args.num_map, args.num_channel, args.num_block, args.num_stage).cuda()
     if args.model_file:
         model.load_state_dict(torch.load(args.model_file))
@@ -127,6 +98,7 @@ if __name__ == '__main__':
         lr_scheduler = MultiStepLR(optimizer, milestones=args.milestone, gamma=0.2)
         results['Loss'] = []
         for epoch in range(1, args.num_iter + 1):
-            train_loss = train_loop(model, train_loader)
+            train_loss = train_loop(model, train_loader, epoch)
             results['Loss'].append(train_loss)
+            lr_scheduler.step()
             save_loop(model, test_loader, epoch)
